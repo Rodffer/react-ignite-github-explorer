2e1aa1e472d2772990e5e8ef984b0a426fe7cf38
Rayron Rodffer <r.rodffer@gmail.com>
2022-02-21 16:28:38 -0300

    Feat: add useEffect para consumir dados da api do github

--- src/components/RepositoryList.jsx
+++ src/components/RepositoryList.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import RepositoryItem from './RepositoryItem';
 
 import '../styles/repositories.scss';
@@ -10,9 +10,17 @@ const repository = {
 }
 
 export default function RepositoryList() {
+  const [repositories, setRepositories] = useState([]);
+
+  useEffect(() => {
+    fetch('https://api.github.com/users/rodffer/repos')
+      .then(res => res.json())
+      .then(data => console.log(data));
+  }, []);
+9
   return (
     <section className='repository-list'>
-      <h1>Repositórios</h1>
+      <h1>Repositórios [rodffer]</h1>
       <ul>
         <RepositoryItem repository={repository}/>
       </ul>
